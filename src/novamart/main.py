7454ae89ef4595bdb01b4d13b653ad348b7be531
2sammy <samuelmanoah61@gmail.com>
2026-07-18 19:34:16 +0300

    Rename platform status function

--- src/novamart/main.py
+++ src/novamart/main.py
@@ -1,7 +1,7 @@
-def display_platform_status():
+def get_platform_status():
     return "NovaMart data platform is running."
 
 
 if __name__ == "__main__":
-    status = display_platform_status()
+    status = get_platform_status()
     print(status)
